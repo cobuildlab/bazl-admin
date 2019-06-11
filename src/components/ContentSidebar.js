b01b6351da056941c92e13ae10cf154a1f473f12
Angel Lacret <alacret@gmail.com>
2019-06-11 15:56:31 -0400

    logout and session

--- src/components/ContentSidebar.js
+++ src/components/ContentSidebar.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { MDBListGroup, MDBListGroupItem, MDBContainer } from "mdbreact";
+import React from 'react';
+import { MDBListGroup, MDBListGroupItem, MDBContainer } from 'mdbreact';
 import { Button } from 'reactstrap';
-import { Link } from "react-router-dom";
-import { onLogout } from '../modules/landing/landing-actions'
-
-import Logo from "../assets/img/Bazl-logo-w.png";
-import { landingStore, LOGOUT_EVENT } from "../modules/landing/landing-store";
+import { Link } from 'react-router-dom';
+import { onLogout } from '../modules/landing/landing-actions';
+import Logo from '../assets/img/Bazl-logo-w.png';
+import { landingStore, LOGOUT_EVENT } from '../modules/landing/landing-store';
+import PropTypes from 'prop-types';
 
 const ContentSidebar = ({ routes }) => {
-
-  React.useEffect((e) => {
-    landingStore.subscribe(LOGOUT_EVENT, (e) => {
-
+  React.useEffect(() => {
+    const logOutsubscription = landingStore.subscribe(LOGOUT_EVENT, () => {
+      window.location.href = '/';
     });
-  }, []);
+    return () => {
+      logOutsubscription.unsubscribe();
+    };
+  });
 
   function isCurrentRoute(pathname) {
     return pathname === window.location.pathname;
   }
+
   return (
     <MDBContainer className="p-0">
       <div className="pl-3 pb-4 pt-3">
@@ -34,21 +37,19 @@ const ContentSidebar = ({ routes }) => {
             </MDBListGroupItem>
           </Link>
         ))}
-      <Link to="/" className="link-logout text-left">
-        Go Out
-      <Button
-        active={false}
-        className="link-logout text-left"
-        onClick={onLogout}
-        color='link'
-        >
-        Go out
-      </Button>
-      </Link>
-
-        </MDBListGroup>
+        <Button
+          active={false}
+          className="link-logout text-left"
+          onClick={onLogout}>
+          Log out
+        </Button>
+      </MDBListGroup>
     </MDBContainer>
   );
+};
+
+ContentSidebar.propTypes = {
+  routes: PropTypes.array.isRequired,
 };
 
 export default ContentSidebar;
