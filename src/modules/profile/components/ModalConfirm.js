0118bf441031e8ba779580670e09480261f453b5
anderjs <andersonav37@gmail.com>
2019-06-11 21:22:18 -0400

    ModalConfirm Component

--- src/modules/profile/components/ModalConfirm.js
+++ src/modules/profile/components/ModalConfirm.js
@@ -1,38 +1,32 @@
-import React from "react";
-import { MDBContainer, MDBModal, MDBModalBody, MDBModalFooter, MDBBtn } from "mdbreact";
-import View from "react-flux-state";
-import Logo from "../../../assets/img/Bazl-logo.png";
+import React from 'react';
+import { MDBContainer, MDBModal, MDBModalBody, MDBModalFooter } from 'mdbreact';
+import PropTypes from 'prop-types';
 
-class ModalComponentTerms extends View {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: this.props.modal
-    };
-  }
+import Logo from '../../../assets/img/Bazl-logo.png';
 
-  render() {
-    const { toggleModal } = this.props;
-    let { modal } = this.state;
-    console.log("Estado de la modal",modal);
-    
-    return (
-      <MDBContainer className="p-0">
-        <MDBModal isOpen={modal} toggle={toggleModal} size="lg">
-          <MDBModalBody className="p-0">
-            <div className="d-flex justify-content-center p-4">
-              <img src={Logo} alt="Bazl" className="img-fluid" />              
-            </div>
-            <h1 className="d-flex justify-content-center p-4">Terms of services and privacy policy</h1>
+/**
+ * @param {{ children: React.ReactChildren, innerText: string, isOpen: boolean, onToggle: () => void  }} Props
+ */
 
-            <MDBModalFooter >
-              <MDBBtn onClick={() => toggleModal()}>Close</MDBBtn>
-            </MDBModalFooter>
-          </MDBModalBody>
-        </MDBModal>
-      </MDBContainer>
-    );
-  };
-}
+const ModalConfirm = ({ children, innerText, isOpen, onToggle }) => (
+  <MDBContainer>
+    <MDBModal isOpen={isOpen} toggle={onToggle && onToggle} size="md">
+      <MDBModalBody className="p-0">
+        <div className="d-flex justify-content-center p-4">
+          <img src={Logo} alt="logo" className="img-fluid" />
+        </div>
+        <h6 className="d-flex justify-content-center p-4">{innerText}</h6>
+      </MDBModalBody>
+      <MDBModalFooter>{children}</MDBModalFooter>
+    </MDBModal>
+  </MDBContainer>
+);
 
-export default ModalComponentTerms;
+ModalConfirm.propTypse = {
+  children: PropTypes.any,
+  innerText: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func,
+};
+
+export default React.memo(ModalConfirm);
