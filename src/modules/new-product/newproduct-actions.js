92be89aadc48d3bef4f0d70dfe4a4d8569067fe5
Salomon <salomom.lugo@gmail.com>
2019-06-12 11:58:19 -0400

    commit

--- src/modules/new-product/newproduct-actions.js
+++ src/modules/new-product/newproduct-actions.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import Flux from 'flux-state';
 import { PRODUCT_EVENT, PRODUCT_ERROR_EVENT } from './newproduct-store';
+import {landingStore, USER_EVENT} from '../landing/landing-store';
 /**
  * creates a new product belonging to the user
  * @param {string} firebaseUser the firebase uid
@@ -12,12 +13,15 @@ export const createProduct = async (product, image)  => {
     const productCollection = DB.collection('products');
     let imageURL =null;
     const storage = firebase.storage();
+    const userData = landingStore.getState(USER_EVENT);
+    const userEmail= userData.email;;
+    console.log(userEmail);
     if(image){
       const storageRef = storage.ref(`/productImages/${image.name}`);
       const task = await storageRef.put(image);
      imageURL = await task.ref.getDownloadURL();
     }
-    console.log(imageURL);
+
       const {
         name,
        category,
@@ -29,7 +33,8 @@ export const createProduct = async (product, image)  => {
         commission,
         additionalFee,
         shippingFee,
-        totalPrice } = product;  
+        totalPrice,
+      user } = product;  
 
        await productCollection.add({
        picture: imageURL,
@@ -43,7 +48,8 @@ export const createProduct = async (product, image)  => {
          commission,
          additionalFee,
          shippingFee,
-         totalPrice
+         totalPrice,
+          user: userEmail
        }).then(doc => {
          console.log("Document writen with ID: ", doc.id);
              Flux.dispatchEvent(PRODUCT_EVENT, doc)
