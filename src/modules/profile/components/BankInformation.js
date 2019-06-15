0c9908572f2bf666dc294ed677aff70a3cce2a91
William Mata <wmata0141@gmail.com>
2019-06-15 14:01:30 -0400

    Mejoras de Diseño

--- src/modules/profile/components/BankInformation.js
+++ src/modules/profile/components/BankInformation.js
@@ -10,8 +10,8 @@ class BankInformation extends React.Component {
       <React.Fragment>
         <div className="mt-3 mb-5">
           <h5>Bank Accounts</h5>
-          <MDBTable bordered>
-            <MDBTableHead>
+          <MDBTable bordered hover responsive>
+            <MDBTableHead color={'primary-plantilla'} textWhite>
               <tr>
                 <th>{'Class'}</th>
                 <th>{'Holder\'s name'}</th>
